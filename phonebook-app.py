9991f232d52c992f3a3e362e4e88bb6c3762c2b5
M. Cagatay Akkiran <109696768+cagatayakk@users.noreply.github.com>
2022-12-11 10:59:42 +0100

    Update phonebook-app.py

--- phonebook-app.py
+++ phonebook-app.py
@@ -6,7 +6,7 @@ from flaskext.mysql import MySQL
 app = Flask(__name__)
 
 # Configure mysql database
-app.config['MYSQL_DATABASE_HOST'] = 'database'
+app.config['MYSQL_DATABASE_HOST'] = 'mycluster'
 app.config['MYSQL_DATABASE_USER'] = 'admin'
 app.config['MYSQL_DATABASE_PASSWORD'] = 'Clarusway_1'
 app.config['MYSQL_DATABASE_DB'] = 'phonebook_db'
